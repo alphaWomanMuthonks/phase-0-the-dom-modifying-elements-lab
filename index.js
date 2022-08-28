6d0af30cadbb4049aa81eb1f6f16053f81bbceca
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-08-28 16:48:28 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code here!
+
+document.querySelector('#main').remove()
+let newHeader = document.createElement('h1')
+newHeader.setAttribute('id','victory')
+newHeader.innerHTML="Joy is the champion"
+const body = document.querySelector('body')
+body.append(h1)
+
+
+
+
+// document.getElementById('main').remove();
+
+// const newHeader = document.createElement('h1')
+// newHeader.id = 'victory'
+// newHeader.innerHTML = "Joy is the champion"
+
+
+// const newHeader = document.querySelector('h1#victory')
